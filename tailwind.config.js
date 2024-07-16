4b9c581526f4aae0f0ffb2dffee0634b8cff2ba2
THE-AUDIO <nandraina.dev22@gmail.com>
2024-07-16 21:24:36 +0300

    responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./*/*.html"],
   theme: {
     extend: {
-      
+      screens: {
+        // Custom breakpoints
+        'android':'320px',
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'lg': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
