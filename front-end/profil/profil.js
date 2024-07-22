8732888b69da051c392aeaf10c05a4d484018e2b
THE-AUDIO <nandraina.dev22@gmail.com>
2024-07-22 23:14:07 +0300

    add bouton flotant

--- front-end/profil/profil.js
+++ front-end/profil/profil.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
     let identifiant = document.getElementById('identifiant')
     let selection = document.getElementById('selection')
     let post = document.getElementById('post')
+    const btn_up = document.getElementById('btn-float')
+    btn_up.style.opacity='0'
     let positionInitial = 200;
 
     function toggleClass() {
@@ -19,9 +21,20 @@ document.addEventListener("DOMContentLoaded", function () {
         if (post.scrollTop >= positionInitial && states) {
                 toggleClass();
                 states = false;
+                btn_up.style.opacity='1'
         } else if (post.scrollTop  <= positionInitial && !states) {
                 states = true;
                 toggleClass();
+                btn_up.style.opacity='0'
+
         }
     });
+
+    btn_up.addEventListener('click', () =>{
+        console.log(1);
+        post.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    })
 })
