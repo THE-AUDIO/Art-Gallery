b45c6d2bb8e9c05bc55ac9e824cfdc1e264ac5e3
THE-AUDIO <nandraina.dev22@gmail.com>
2024-07-22 16:55:48 +0300

    add profil page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*/*.html"],
+  content: [
+    "./*/*/*.html",
+    "./frontend/profil/profil.html",
+  ],
   theme: {
     extend: {
       screens: {
