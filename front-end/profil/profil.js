27732c8533f2da0511a5e02afbdfcfedca21f00b
THE-AUDIO <nandraina.dev22@gmail.com>
2024-07-22 22:21:15 +0300

    version 1.0 du animation scrol

--- front-end/profil/profil.js
+++ front-end/profil/profil.js
@@ -2,7 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     let container_profil = document.getElementById('profil-container');
-    let btn = document.getElementById('up')
     let identifiant = document.getElementById('identifiant')
     let selection = document.getElementById('selection')
     let post = document.getElementById('post')
@@ -15,10 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
         post.classList.toggle('up-container')
         post.classList.toggle('up-post');
     }
-    btn.addEventListener('click', toggleClass)
     var states = true;
     post.addEventListener('scroll', () => {
-        console.log(post.scrollTop);
         if (post.scrollTop >= positionInitial && states) {
                 toggleClass();
                 states = false;
