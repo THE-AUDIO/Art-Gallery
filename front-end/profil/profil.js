62e9cc67250d94608667e7ac696c978fcb78df29
THE-AUDIO <nandraina.dev22@gmail.com>
2024-07-24 21:15:34 +0300

    ajust some begue

--- front-end/profil/profil.js
+++ front-end/profil/profil.js
@@ -1,36 +1,24 @@
 
-
+// await the document load  
 document.addEventListener("DOMContentLoaded", function () {
+    // declaration of the html element
+    const btn_up = document.getElementById('btn-float')
     let container_profil = document.getElementById('profil-container');
     let identifiant = document.getElementById('identifiant')
     let selection = document.getElementById('selection')
     let post = document.getElementById('post')
-    const btn_up = document.getElementById('btn-float')
     const saveBtn = document.getElementById('save-btn')
     const closeBtn = document.getElementById('close')
     const showEditProfilBtn = document.getElementById('btn-edit-profil')
     let contentEditProfil = document.querySelector('.edit-profil');
-    btn_up.style.opacity = '0'
+    let btnAddPost = document.getElementById('section-btn-add-post');
+    let addPostSection = document.getElementById('add-post');
+    let btnSavePost = document.getElementById('btn-add-post');
     let positionInitial = 200;
 
+// all functions is here
 
-    function hiddenEditProfil() {
-        post.style.filter = 'blur(0)'
-        contentEditProfil.style.display = 'none';
-        selection.style.filter = 'blur(0)'
-        container_profil.style.filter = 'blur(0)'
-    }
-    closeBtn.addEventListener('click', hiddenEditProfil);
-    saveBtn.addEventListener('click', hiddenEditProfil);
-    showEditProfilBtn.addEventListener('click', () => {
-        contentEditProfil.style.display = 'flex';
-        post.style.filter = 'blur(2px)'
-        selection.style.filter = 'blur(2px)'
-        container_profil.style.filter = 'blur(2px)'
-
-        // contentEditProfil.style.transform='scale(1)'
-    })
-
+    // this function is used to move up the container
     function toggleClass() {
         container_profil.classList.toggle('up-container')
         identifiant.classList.toggle('up-container')
@@ -38,7 +26,50 @@ document.addEventListener("DOMContentLoaded", function () {
         post.classList.toggle('up-container')
         post.classList.toggle('up-post');
     }
+
+    function blurSomeDiv(){
+        post.style.filter = 'blur(2px)'
+        selection.style.filter = 'blur(2px)'
+        container_profil.style.filter = 'blur(2px)'
+    }
+
+    function unBlurSomeDiv(){
+        post.style.filter = 'blur(0)'
+        selection.style.filter = 'blur(0)'
+        container_profil.style.filter = 'blur(0)'
+    }
+    function hiddenEditProfil() {
+        contentEditProfil.style.display = 'none';
+        unBlurSomeDiv();
+    }
+
+    function hiddenAddPost(){
+        addPostSection.classList.add('add-post');
+        console.log('hidden add post')
+        unBlurSomeDiv()
+    }
+
+// all event is here .......
+
+    btnAddPost.addEventListener('click', ()=>{
+        addPostSection.classList.remove('add-post');
+        blurSomeDiv();
+    })
+    btnSavePost.addEventListener('click', hiddenAddPost)
+    closeBtn.addEventListener('click', hiddenEditProfil);
+    saveBtn.addEventListener('click', hiddenEditProfil);
+
+    
+    // here we make visible a edit profil section
+    showEditProfilBtn.addEventListener('click', () => {
+        contentEditProfil.style.display = 'flex';
+        blurSomeDiv();
+    })
+
+// we listen a scroll event in the div post and call the toogle function
+// if some condition is verified
     var states = true;
+    btn_up.style.opacity = '1';
     post.addEventListener('scroll', () => {
         if (post.scrollTop >= positionInitial && states) {
             toggleClass();
@@ -48,9 +79,11 @@ document.addEventListener("DOMContentLoaded", function () {
             states = true;
             toggleClass();
             btn_up.style.opacity = '0'
-
         }
     });
+
+
+// here if we at the post section this function is used to go back to the previous section
 
     btn_up.addEventListener('click', () => {
         console.log(1);
